4df7be39e317e6814273762b45be3dbf0f03cffa
alistairjoelquinn <alistairjoelquinn@gmail.com>
2023-04-11 21:20:33 +0200

    adds basic library structure

---
+++ src/player/components.rs
@@ -0,0 +1,6 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct Player {
+    pub score: u32,
+}
---
+++ src/player/systems.rs
@@ -0,0 +1,22 @@
+pub const PLAYER_SPEED: f32 = 300.0;
+
+pub fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(
+                window.width() / 2.0,
+                window.height() / 2.0,
+                0.0,
+            ),
+            texture: asset_server.load("sprites/ball_blue_large.png"),
+            ..default()
+        },
+        Player {},
+    ));
+}
