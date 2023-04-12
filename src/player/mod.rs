bf2350f6264a93c89ca8f16e9330f41a34f37d39
alistairjoelquinn <alistairjoelquinn@gmail.com>
2023-04-12 20:09:48 +0200

    adds player plugin

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -1,1 +1,16 @@
+use bevy::prelude::*;
 
+pub mod components;
+pub mod systems;
+
+use systems::*;
+
+pub struct PlayerPlugin;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(spawn_player)
+            .add_system(player_movement)
+            .add_system(confine_player_movement);
+    }
+}
