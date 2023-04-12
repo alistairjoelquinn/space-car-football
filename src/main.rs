d973f3b6c7567e65cc315640947364e79af23bca
alistairjoelquinn <alistairjoelquinn@gmail.com>
2023-04-12 20:32:47 +0200

    updates background

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use car_football::player::PlayerPlugin;
 
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::DARK_GREEN))
         .add_plugins(DefaultPlugins)
         .add_plugin(PlayerPlugin)
         .add_startup_system(spawn_camera)
