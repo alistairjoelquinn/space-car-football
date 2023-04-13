a376e57b6f57e6a8daa7670c6bdc3abd8c5c3add
alistairjoelquinn <alistairjoelquinn@gmail.com>
2023-04-13 19:58:40 +0200

    adds window config and rapier

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 use bevy::prelude::*;
 
+use bevy::window::PresentMode;
 use car_football::game::systems::spawn_camera;
 use car_football::player::PlayerPlugin;
 
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::DARK_GREEN))
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "I am a window!".into(),
+                resolution: (1200., 700.).into(),
+                present_mode: PresentMode::AutoVsync,
+                // Tells wasm to resize the window according to the available canvas
+                fit_canvas_to_parent: true,
+                // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugin(PlayerPlugin)
         .add_startup_system(spawn_camera)
         .run();
