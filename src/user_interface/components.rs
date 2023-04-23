cff485a08c0ac56316ee1733b53ba5f1d00a8573
alistairjoelquinn <alistairjoelquinn@gmail.com>
2023-04-23 15:12:27 +0200

    adds ui components for building a loading and menu screen:

---
+++ src/user_interface/components.rs
@@ -0,0 +1,7 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct Loading {}
+
+#[derive(Component)]
+pub struct Menu {}
---
+++ src/user_interface/systems.rs
@@ -0,0 +1,7 @@
+pub mod create_loading_screen() {};
+
+// pub mod create_menu_screen() {};
+
+// pub mod create_game_screen() {};
+
+// pub mod create_game_over_screen() {};
